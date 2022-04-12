d2abfca2844b24749e875972fc6fd823abc4a6bc
Jehovanie <jehovanieram@gmail.com>
2022-04-12 12:57:08 +0300

    add file data.js be fake data

---
+++ src/data.js
@@ -0,0 +1,78 @@
+const data = [
+    {
+        car: [
+            {
+                name: "Lorem 1",
+                image: "car/model-1",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 2",
+                image: "car/model-2",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 3",
+                image: "car/model-3",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 4",
+                image: "car/model-4",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+
+        ]
+    },
+    {
+        informatique: [
+            {
+                name: "Lorem 1",
+                image: "informaitque/model-1",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 2",
+                image: "informaitque/model-2",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 3",
+                image: "informaitque/model-3",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 4",
+                image: "informaitque/model-4",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+
+        ]
+    },
+    {
+        scouter: [
+            {
+                name: "Lorem 1",
+                image: "scouter/model-1",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 2",
+                image: "scouter/model-2",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 3",
+                image: "scouter/model-3",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+            {
+                name: "Lorem 4",
+                image: "scouter/model-4",
+                descrition: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi dolorem fugit vitae laudantium in!"
+            },
+
+        ]
+    }
+
+]
